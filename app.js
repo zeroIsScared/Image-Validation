c6659ec042dae92b5ccf5110b2ffab32f4d041ba
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-06-23 16:19:22 +0300

    Add files via upload
    added some changes

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const formUpload = document.querySelector("#form-upload");
 const inputFile = document.querySelector("#form-upload input");
 const previewImg = document.querySelector("#form-upload img");
 
-const onUpload = (e)=> {
+let model = null;
+
+cocoSsd.load().then(loadedModel => {
+    model = loadedModel;
+    console.log(model);
+})
+
+const onUpload = (e) => {
     console.dir(inputFile);
     // get the file info
     let fileInfo = inputFile.files[0];
@@ -16,6 +23,22 @@ const onUpload = (e)=> {
 
     //show the image element
     previewImg.style.display = 'inline-block';
+
+    //detect object in our Image
+    setTimeout(() => {
+        previewImg.width = previewImg.offsetWidth;
+        previewImg.height = previewImg.offsetHeight;
+        model.detect(previewImg).then(predictions => {
+            console.log("Predictions:", predictions);
+            for (let i = 0; i < predictions.length; i++) {
+                if (predictions[i].class === 'cat' && predictions[i].score >= 0.5) {
+                    alert("This image upload was canceled it contains a cat not a person!")
+                }
+            }
+
+        })
+    }, 500)
+
 }
 
 inputFile.addEventListener("change", onUpload);
